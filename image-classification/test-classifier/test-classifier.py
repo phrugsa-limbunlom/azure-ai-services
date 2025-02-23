a49faaa3633945249d6a309065ee1dbf5b527d87
phrugsa-limbunlom <phrugsa.lim@gmail.com>
2025-02-23 20:38:59 +0000

    training and testing cutom image classification

---
+++ image-classification/test-classifier/test-classifier.py
@@ -0,0 +1,35 @@
+from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
+from msrest.authentication import ApiKeyCredentials
+import os
+from dotenv import load_dotenv, find_dotenv
+
+def main():
+
+    try:
+        # Get Configuration Settings
+        load_dotenv(find_dotenv())
+        prediction_endpoint = os.getenv('PredictionEndpoint')
+        prediction_key = os.getenv('PredictionKey')
+        project_id = os.getenv('ProjectID')
+        model_name = os.getenv('ModelName')
+
+        # Authenticate a client for the training API
+        credentials = ApiKeyCredentials(in_headers={"Prediction-key": prediction_key})
+        prediction_client = CustomVisionPredictionClient(endpoint=prediction_endpoint, credentials=credentials)
+
+        # Classify test images
+        for image in os.listdir('test-images'):
+            image_data = open(os.path.join('test-images',image), "rb").read()
+            results = prediction_client.classify_image(project_id, model_name, image_data)
+            print(results)
+
+            # Loop over each label prediction and print any with probability > 50%
+            for prediction in results.predictions:
+                if prediction.probability > 0.5:
+                    print(image, ': {} ({:.0%})'.format(prediction.tag_name, prediction.probability))
+    except Exception as ex:
+        print(ex)
+
+if __name__ == "__main__":
+    main()
+
